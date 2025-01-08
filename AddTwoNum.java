e4bca2985775aae1fa9117e80ccf6777da418564
gopal-krishna-jha <gopalkrishnajha06@gmail.com>
2025-01-08 10:39:53 +0530

    first commit

---
+++ AddTwoNum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddTwoNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter First Number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter Second Number: ");
+        int num2 = sc.nextInt();
+        System.out.println("Sum of the Number is: "+ (num1+num2));
+    }
+
+}
